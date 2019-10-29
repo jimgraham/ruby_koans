bda52d12a51470d66577b311a7b5616c664f79cf
Jim Graham <jim.graham@shopify.com>
2019-10-29 18:10:28 -0400

    Finish the second set of triangle code

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,8 +15,29 @@ require 'set'
 #   about_triangle_project_2.rb
 #
 
+
 def triangle(a, b, c)
-  sides = Set[a, b, c]
+  sides = [a, b, c]
+
+  if sides.any? { |s| s <= 0 }
+    raise TriangleError
+  end
+
+  # https://en.wikipedia.org/wiki/Triangle_inequality
+  sides.each do |s|
+    other_sides = sides.clone
+    other_sides.delete_at(sides.index(s))
+
+    # `.sum` not in Ruby 2.3
+    sum_other_sides = 0.0
+    other_sides.each { |s| sum_other_sides += s }
+
+    if sum_other_sides <= s
+      raise TriangleError
+    end
+  end
+
+  sides = Set.new(sides)
 
   case sides.size
     when 1
